0c9958865237cf349f0f4e6685b7ccdf377219ed
yashwantpawar <56951870+yashwantpawar@users.noreply.github.com>
2024-01-06 22:35:33 +0530

    Add files via upload

---
+++ shell script/create_files.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# example
+
+# create 5 files using for loop
+for i in {1..5}; do touch file$i.txt; done
+
+# create 5 directories using for loop
+for i in {1..5}; do mkdir dir$i; done
